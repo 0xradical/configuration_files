f34e3b0701737a409cde4755bcfae65324ec7a2a
thiagobrandam <thiago@redealumni.com>
2011-08-01 12:28:03 -0300

    added line to fetch git completion

--- install_stuff.sh
+++ install_stuff.sh
@@ -1,3 +1,7 @@
 
 # installing cpanminus (cpanm) to facilitate perl modules installs
 cpan App::cpanminus
+
+
+# fetching git completion
+curl https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -OL
