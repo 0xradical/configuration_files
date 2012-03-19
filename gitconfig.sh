2cb5165c175b13bfa2bec4ec65cc986f823103fd
thiagobrandam <thiago@redealumni.com>
2012-03-19 13:47:26 -0300

    added config for git

---
+++ gitconfig.sh
@@ -0,0 +1,5 @@
+git add --global user.name 'thiagobrandam'
+git add --global user.email 'thiago@redealumni.com'
+git add --global github.user 'thiagobrandam'
+git add --global color.ui 'auto'
+git add --global push.default 'upstream'
